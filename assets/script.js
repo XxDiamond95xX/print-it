48bd7abd0c63ae2abe6be9c67b053256ffd1ed04
XxDiamond95xX <143485787+XxDiamond95xX@users.noreply.github.com>
2023-11-15 06:18:58 +0100

    ajout de toutes les constantes principales

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,38 @@
+
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+    {
+        "image": "slide1.jpg",
+        "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+        "image": "slide2.jpg",
+        "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+        "image": "slide3.jpg",
+        "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+        "image": "slide4.png",
+        "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+    }
+];
 
-const left = document.querySelector(`.arrow_left`);
-const right = document.querySelector(`.arrow_right`);
-const dots = document.querySelectorAll(`.dot`);
-const bannerImg = document.querySelector(`.banner-img`);
+// constante lié a la bannière, aux flèches et aux points 
+const bannerImg = document.querySelector('.banner-img')
+const arrowLeft = document.querySelector('.arrow_left')
+const arrowRight = document.querySelector('.arrow_right')
+const dots = document.querySelectorAll('.dot') 
 
+// constante lié à l'image
+const imgSlide = `assets/images/slideshow/${slides[currentIndex].jpg}`
+bannerImg.src = imgSlide
+bannerImg.alt = `slide ${currentIndex + 1} `
 
-left.addEventListener("click", function() {
-	console.log("clic sur fleche de gauche")
-});
+// constante lié a la tagLine
+const tagLine = slides[currentIndex].tagLine
+document.querySelector('p').innerHTML = tagLine
 
-right.addEventListener("click", function() {
-	console.log("clic sur fleche de droite")
-});
-
-let position = slides [0];
-
-
-
-
+// Paramètre de démarrage de l'index
+let currentIndex = 0
 
